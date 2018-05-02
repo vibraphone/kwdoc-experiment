38258e6461acfb217e6eea6a528ba17fc1c78e76
David Thompson <david.thompson@kitware.com>
2018-05-02 04:58:29 -0400

    Test.

---
+++ documentation/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  baseUrl: '/kwdoc-experiment',
+  work: './build-tmp',
+  config: {
+    title: 'sensei',
+    description: '"Scalable in situ visualization and analysis."',
+    subtitle: '"Lightweight, zero-copy simulation adaptor."',
+    author: 'SENSEI',
+    timezone: 'UTC',
+    url: 'https://vibraphone.github.io/kwdoc-experiment',
+    root: '/kwdoc-experiment/',
+    github: 'vibraphone/kwdoc-experiment',
+  }
+};
