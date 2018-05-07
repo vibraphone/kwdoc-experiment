73a77d14b6a13501a200649f522f68a21cd24942
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-05-07 08:58:09 -0600

    add cname

--- documentation/config.js
+++ documentation/config.js
@@ -2,6 +2,7 @@ module.exports = {
   baseUrl: '',
   work: './build-tmp',
   config: {
+    cname: 'vibraphone.github.io',
     title: 'sensei',
     description: '"Scalable in situ visualization and analysis."',
     subtitle: '"Lightweight, zero-copy simulation adaptor."',
